45844f6edda746afb8824ea3abbaa10b38ab4aab
Chien-Tai Cheng <htaiwan@yahoo-inc.com>
2016-04-25 21:33:01 +0800

    Add front-end form to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,19 @@ socket.on('message', function (message) {
 	console.log('New message:');
 	console.log(message.text);
 });
+
+// 處理新訊息傳送
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	// 清除input的訊息內容
+	$message.val('');
+});
