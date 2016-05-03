48c69e1b0ec05fe8e76a8322568d3bdcf7a1db8b
Chien-Tai Cheng <htaiwan@yahoo-inc.com>
2016-05-03 21:05:57 +0800

    Timestamps

--- moment-example.js
+++ moment-example.js
@@ -2,6 +2,13 @@ var moment = require('moment');
 var now = moment();
 
 console.log(now.format());
+console.log(now.format('X'));  // timestamp s
+console.log(now.format('x'));  // timestamp ms
+
+var timestamp = 132324435343;
+var timestampMoment = moment.utc(timestamp);
+
+console.log(timestampMoment.format('h:mm a')); // 11:06 am
 
 now.subtract(1, 'year');
 
