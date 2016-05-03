603ceb9e51ddee864bd587e70f4fe98e7ade40d1
Chien-Tai Cheng <htaiwan@yahoo-inc.com>
2016-05-03 20:49:53 +0800

    Working With Time

---
+++ moment-example.js
@@ -0,0 +1,11 @@
+var moment = require('moment');
+var now = moment();
+
+console.log(now.format());
+
+now.subtract(1, 'year');
+
+console.log(now.format());
+
+console.log(now.format('MMM Do YYYY, h:mm a')); // Oct 5th 2015, 6:45 pm
+
